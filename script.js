1f6ef4037453d7beb8157d17502d2318b63d5c19
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-03-24 12:41:25 +0300

    Первый урок

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let title = "Заголовок";
+const screens = "Скрины";
+let screenPrice = 20;
+let rollback = 10;
+let fullPrice = 20;
+let adaptive = 30;
+
+alert('Текст из модального окна');
+console.log('Текст из консоли');
