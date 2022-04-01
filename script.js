7ff82d815b7cf3b7096928c69084a5fe6b959fe6
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-01 11:51:26 +0300

    урок 7

--- script.js
+++ script.js
@@ -15,75 +15,72 @@ let allServicePrices = 0;
 let servicePercentPrice = 0;
 
 
-function isNumber(num){
-   return !isNaN(parseFloat(num) && isFinite(num));
+function isNumber(num) {
+  return !isNaN(parseFloat(num) && isFinite(num));
 }
 
-function asking(){
+function asking() {
   title = prompt("Как называется ваш проект?", "Космос");
   screens = prompt("Какие типы экранов нужно разработать?", "Мониторы, планшеты, телефоны");
 
   do {
-    // Я поставил знак + перед prompt и теперь в переменню всегда записывается число.
-    // Если ввести с пробелами, они обрезаюся и записывается только число.
-    // В случае нажатия Отмена, в переменная принимает значение 0.
     screenPrice = +prompt("Сколько будет стоить данная работа?", "5000");
   } while (!isNumber(screenPrice));
   adaptive = confirm("Нужен ли адаптив на сайте?");
 }
 
-function getAllServicePrices(){
+function getAllServicePrices() {
   let price = 0;
   let price1 = 0;
   let i = 0;
-  do{     
-      prompt("Какой дополнительный тип услуги нужен?", "Отправка писем");
+  do {
+    prompt("Какой дополнительный тип услуги нужен?", "Отправка писем");
+    price = +prompt("Сколько это будет стоить?", "100");
+    while (!isNumber(price)) {
       price = +prompt("Сколько это будет стоить?", "100");
-      while (!isNumber(price)){
-        price = +prompt("Сколько это будет стоить?", "100");  
-      }
-      price1 += price;
-      i++;     
+    }
+    price1 += price;
+    i++;
   } while (i < 2);
   return price1;
 }
 
-function showTypeOf(variable){
+function showTypeOf(variable) {
   console.log("Тип: " + variable, typeof variable);
 }
 
-function getFullPrice(price1, price2){
+function getFullPrice(price1, price2) {
   return price1 + price2;
 }
 
-function getTitle(str){
+function getTitle(str) {
   let words = str.trim().split(" ");
 
-  for (let i = 0; i < words.length; i++) { 
+  for (let i = 0; i < words.length; i++) {
     words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
-   }
+  }
 
   return words.join(" ");
 }
 
-function getServicePercentPrices(price){
+function getServicePercentPrices(price) {
   return Math.round(price - (price * (rollback / 100)));
 }
 
-function getRollbackMessage (price){
-  if (price > 30000){
+function getRollbackMessage(price) {
+  if (price > 30000) {
     return "Даем скидку в 10%";
-  } else if (price >= 15000 && price <=30000){
+  } else if (price >= 15000 && price <= 30000) {
     return "Даем скидку в 5%";
-  } else if (price < 15000 && price >=0){ 
+  } else if (price < 15000 && price >= 0) {
     return "Скидка не предусмотрена";
-  } else if(price < 0){
+  } else if (price < 0) {
     return "Что то пошло не так";
   }
 }
 
 asking();
-allServicePrices = getAllServicePrices(); 
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice(screenPrice, allServicePrices);
 servicePercentPrice = getServicePercentPrices(fullPrice);
 title = getTitle(title);
@@ -105,7 +102,3 @@ console.log("Типы экранов для разработки: " + screens);
 console.log(getRollbackMessage(fullPrice));
 console.log("Стоимость за вычетом процента отката посреднику: " + getServicePercentPrices(fullPrice) + " $");
 console.log("Стоимость дополнительных услуг: " + allServicePrices + " $");
-
-
-
-
